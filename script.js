2138e12d355b486526b4ab8cbcf4fbf38d51951c
sarath629 <124670726+sarath629@users.noreply.github.com>
2023-03-16 10:57:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 // Your Script here.
+function rot13(str) {
+	string ans="";
+	for (let index = 0; index < str.length; index++) {
+		string ch=str.charAt(index);
+		string add=ch+13;
+		if(add>='a' && add<='z'){
+			var no=add-'a';
+			add='A'+no;
+		}
+		ans+=add;
+		
+	}
+	return ans;
+	
+}
